11a6c8ed1608bf011ab8bffff0fe68197b1cf67f
Tozzi <nicholastozzi@gmail.com>
2023-01-19 15:04:01 -0500

    Finished basic readME generator

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,131 @@
+license = "";
+//returns badge image, and on click provides the license link
+function renderlicenseInfo(license) {
+  switch (license) {
+    case "GNU AGPLv3":
+      return (license =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)");
+    case "GNU GPLv3":
+      return (license =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)");
+
+    case "GNU LGPLv3":
+      return (license =
+        "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)");
+
+    case "Mozilla Public License 2.0":
+      return (license =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)");
+
+    case "Apache License 2.0":
+      return (license =
+        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)");
+
+    case "MIT License":
+      return (license =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
+
+    case "Boost Software License 1.0":
+      return (license =
+        "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)");
+
+    case "The Unlicense":
+      return (license =
+        "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)");
+    case "Other":
+      return (license =
+        "[![License: TYPE LICENSE NAME HERE](PROVIDE BADGE IMAGE SRC LINK HERE)](PROVIDE BADGE LINK HERE)");
+  }
+}
+
+function renderBadgeSection(answers) {
+  if (answers.license !== "None") {
+    return `${renderlicenseInfo(answers.license)}`;
+  } else {
+    return "";
+  }
+}
+
+//returns the license section of README
+function renderLicenseSection(answers) {
+  if (answers.license !== "None") {
+    return `## License
+  ${answers.license}
+  ${renderlicenseInfo(answers.license)}
+  `;
+  } else {
+    return "";
+  }
+}
+
+//generate markdown for README
+const generateMarkdown = (answers) => {
+  return `
+  # ${answers.title} ${renderBadgeSection(answers)}
+
+  ## Description
+  \`\`\`
+  ${answers.description}
+  \`\`\`
+  ## Badges
+   ${answers.license}
+  
+  
+  ## Table Of Contents
+  
+[Authors](#Authors) <br />
+[Installation](#installation)<br />
+[Usage](#Usage)<br />
+[License](#License)<br />
+[Contributing](#Contributing)<br />
+[Tests](#Tests)<br />
+[Questions](#Questions)<br />
+
+  ## Authors
+  
+[ ${answers.username}](https://www.github.com/${answers.username})
+
+
+  ## Installation
+  
+  ${answers.installation}
+ 
+  bash
+    npm install 
+    cd 
+      
+  ## Usage
+  ${answers.usage}
+
+  ${renderLicenseSection(answers)}
+  
+  
+  
+  ## Contributing
+  
+  ${answers.contribution}
+  Contributions are always welcome!
+  
+  
+  
+  ##Tests
+  
+  ${answers.instructions} 
+  
+  
+  ## Questions
+ 
+  Have any questions? Feel free to reach ${answers.username} </br>
+  at ${answers.email} </br>
+  or [Twitter]("twitter.com/${answers.twitter}")</br>
+  
+  
+
+  ## Pages
+  [Check out this project on GitHub Pages]("https://${
+    answers.username
+  }.github.io/${answers.title}/")</br>
+    `;
+};
+//exports
+module.exports = generateMarkdown;
---
+++ Develop/utils/index.js
@@ -0,0 +1,79 @@
+// TODO: Create a function to initialize app
+// function init() {}
+// init();
+
+//packages required
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
+//readME prompt
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Add a description for your README",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Add any installation instructions",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Add any usage information",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Add any contribution guidelines",
+      name: "contribution",
+    },
+    {
+      type: "input",
+      message: "Add any test constructions",
+      name: "instructions",
+    },
+    {
+      type: "list",
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla Public License 2.0",
+        "Apache License 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "The Unlicense",
+        "Other",
+        "None",
+      ],
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "What is the title of your GitHub repository?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is your twitter?",
+      name: "twitter",
+    },
+  ])
+  .then((answers) => {
+    const readmeContent = generateMarkdown({ ...answers });
+    fs.writeFile("README.md", readmeContent, (err) => {
+      err ? console.log(err) : console.log("Successfully created README");
+    });
+  });
